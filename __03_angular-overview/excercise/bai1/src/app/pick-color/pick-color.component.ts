3e6bc95e8255f4e23cf4568b8b7dee1025a61e33
thanhtai18021994 <taibui18021994@gmail.com>
2021-06-06 18:32:03 +0700

    update excercise

--- __03_angular-overview/excercise/bai1/src/app/pick-color/pick-color.component.ts
+++ __03_angular-overview/excercise/bai1/src/app/pick-color/pick-color.component.ts
@@ -6,12 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pick-color.component.css']
 })
 export class PickColorComponent implements OnInit {
-color: string='#000';
-  constructor() { }
+color: string='#634a82';
+red=0;
+green=0;
+blue=0;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-  takeColor(param){
-    this.color=param;
+
+  change():void{
+    let red=this.red.toString(16)
+    let green=this.green.toString(16)
+    let blue=this.blue.toString(16)
+    if(red.length===1){
+      red='0'+red;
+    }
+    if(green.length===1){
+      green='0'+green;
+    }
+    if(blue.length===1){
+      blue='0'+blue;
+    }
+    let color='#'+red+green+blue;
+    this.color=color;
   }
 }
